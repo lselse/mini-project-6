2648468ebab663608bdedf074f849d684d47b63e
lselse <lselse8112@gmail.com>
2026-06-23 23:11:15 +0900

    url 수정

--- backend/src/main/resources/generate-data-sql.mjs
+++ backend/src/main/resources/generate-data-sql.mjs
@@ -13,7 +13,7 @@
 
 import { writeFileSync } from 'node:fs';
 
-const BACKEND_URL = 'http://localhost:8080/books';
+const BACKEND_URL = 'http://a46a72be6fcc7475f8c39c3556dba827-1702841606.us-west-1.elb.amazonaws.com/books';
 const OUTPUT_PATH = './data.sql';
 
 function escapeSql(text) {
